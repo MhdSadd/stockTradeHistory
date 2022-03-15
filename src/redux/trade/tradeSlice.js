3f9b979bfea89202a4bb4c26b86a132d6ddd1a4e
MSadiq <mhdsadd3@gmail.com>
2022-03-15 18:19:59 +0100

    init

---
+++ src/redux/trade/tradeSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { FaLessThanEqual } from "react-icons/fa";
+
+
+
+const initialState = {
+  market: '',
+  grossProfit: '',
+  grossLoss: '',
+  stocks: null,
+    consideration: "",
+    quantity: "",
+    email: "",
+    price:'',
+    direction: '',
+    changeInPriceFromAToB: '',
+    date: '',
+    isLoadingT: FaLessThanEqual
+
+  }
+  
+  const tradeSlice = createSlice({
+    name: "trade",
+   initialState,
+    reducers: {
+      tradePending: (state) => {
+        state.isLoadingT = true;
+      },
+      tradeSuccess: (state, { payload }) => {
+        console.log(payload);
+        state.stocks = payload.stock;
+        state.grossLoss = payload.grossLoss;
+        state.grossProfit = payload.grossProfit;
+        state.market = payload.market
+
+        state.isLoadingT = false;
+        // return state;
+      },
+      tradeFail: (state, { payload }) => {
+        console.log('payload', payload);
+        state.isLoading = false;
+      },
+     
+    },
+  
+  })
+  
+  export default tradeSlice;
+  
+  export const userSelector = state => state.trade;
+  
